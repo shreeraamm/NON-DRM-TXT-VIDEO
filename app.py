5863164e75ab4db52bd12408f803e568160fe2c7
Popeye <127945393+Popeye68@users.noreply.github.com>
2025-04-24 13:32:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,47 +6,43 @@ def hello_world():
     return """
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" type="text/css" href="https://tinypic.host/images/2025/02/07/DeWatermark.ai_1738952933236-1.png">
-    <titleSudoR2spr Repository</title>
-	<link rel="icon" type="image/x-icon" href="https://tinypic.host/images/2025/02/07/DeWatermark.ai_1738952933236-1.png">
-
+    <title>Spidy Repo</title>
+    <link rel="icon" type="image/x-icon" href="https://tinypic.host/images/2025/04/23/1000058493.jpg">
 </head>
 
-<body>
-    <div class="container" style="bg-dark text-red text-center py-3 mt-5">
-        <a href="https://github.com/nikhilsaini098" class="card">
+<body style="background-color: #000; color: #fff; font-family: monospace; text-align: center; padding-top: 60px;">
+    <div class="container">
+        <a href="https://github.com/nikhilsaini098" style="text-decoration: none; color: #ff3c00;">
             <p>
-              />▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄<br 
-	      />██░▄▄▄░█░▄▄▀█▄░▄██░▀██░█▄░▄██<br 
-              />██▄▄▄▀▀█░▀▀░██░███░█░█░██░███<br 
-	      />██░▀▀▀░█░██░█▀░▀██░██▄░█▀░▀██<br 
-              />▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀<br />
-                                             <br>
+    />▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄<br>
+    />███░████░████░████░███░██░██<br>
+    />███░███░░███░░███░░███░██░██<br>
+    />███░███░░███░░███░░███░██░██<br>
+    />▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀<br><br>
+    <b style="font-size: 18px;">v2.0.0</b>
+</p>
 
-                <b>v2.0.0</b>
-            </p>
         </a>
     </div>
-	<br></br><br></br><br></br>
-	<footer class="bg-dark text-white text-center py-3 mt-5">
-	<center><img loading="lazy" class="object-none object-center" src="https://tinypic.host/images/2025/02/07/DeWatermark.ai_1738952933236-1.png" width="240" height="120">
-        Powered By SAINI 
-		<img loading="lazy" class="object-none object-center" src="https://tinypic.host/images/2025/02/07/DeWatermark.ai_1738952933236-1.png" width="240" height="120">
-		<div class="footer__copyright">
-            <p class="footer__copyright-info">
-                © 2024 Video Downloader. All rights reserved.
-            </p>
-        </div>
-    </footer></center>
-</body>
 
+    <br><br><br>
+
+    <footer style="color: white; margin-top: 80px;">
+        <center>
+            <img src="https://tinypic.host/images/2025/04/23/1000058493.jpg" width="240" height="120" alt="Spidy Logo">
+            <h3>Powered By SPIDY</h3>
+            <img src="https://tinypic.host/images/2025/04/23/1000058493.jpg" width="240" height="120" alt="Spidy Logo">
+            <div>
+                <p style="margin-top: 10px;">© 2024 Video Downloader. All rights reserved.</p>
+            </div>
+        </center>
+    </footer>
+</body>
 </html>
 """
-
 
 if __name__ == "__main__":
     app.run()
